63d2ab8e189c6a18385686f6811f02990161d94e
Austin Abell <austinabell8@gmail.com>
2023-02-23 21:42:17 +0100

    very basic and static connected working example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import { seal } from "./proof";
+
+const journalBase64 = "AAAAAINJFKY";
+const idBase64 = "84keoVl3RAHWa4hlfV/q0f3opURNbbSp/7jU7qJ5Ft8";
 
 function App() {
+  const [text, setText] = useState("Validating proof...");
+
   useEffect(() => {
+    const journal = Buffer.from(journalBase64, "base64");
+    const sealBytes = new Uint32Array(Buffer.from(seal, "base64").buffer);
+    const id = Buffer.from(idBase64, "base64");
     import("risc0-js").then((module) => {
-      module.greet("WebAssembly");
+      try {
+        module.validate_proof(journal, sealBytes, id);
+        setText("Proof is valid");
+      } catch (e) {
+        setText("Error: " + e);
+        console.error(e);
+      }
     });
   }, []);
-  return <div className="App">Ignore</div>;
+  return <div className="App">{text}</div>;
 }
 
 export default App;
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
+use risc0_zkvm::{sha::Digest, Receipt};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn validate_proof(journal: &[u8], seal: &[u32], image_id: &[u8]) -> Result<(), JsError> {
+    let receipt: Receipt = Receipt::new(journal, seal);
+    let image_id: [u8; 32] = image_id.try_into()?;
+    let digest = Digest::from(image_id);
+    Ok(receipt
+        .verify(&digest)
+        .map_err(|e| JsError::new(&format!("Failed to validate proof: {}", e)))?)
 }

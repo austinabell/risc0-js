76e020957f3f01177d2a8e37620eb479969e4094
Austin Abell <austinabell8@gmail.com>
2023-02-23 22:25:31 +0100

    add code for generating proof in js

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { seal } from "./proof";
+import { seal, code } from "./inputs";
 
 const journalBase64 = "AAAAAINJFKY";
 const idBase64 = "84keoVl3RAHWa4hlfV/q0f3opURNbbSp/7jU7qJ5Ft8";
@@ -14,8 +14,16 @@ function App() {
     const id = Buffer.from(idBase64, "base64");
     import("risc0-js").then((module) => {
       try {
-        module.validate_proof(journal, sealBytes, id);
+        module.validate_proof(new module.Receipt(journal, sealBytes), id);
         setText("Proof is valid");
+
+        // const codeBytes = Buffer.from(code, "base64");
+        // const input = new Uint32Array(
+        //   Buffer.from("AACt3gAAAAAAAO++AAAAAA", "base64")
+        // );
+        // const genRec = module.generate_proof(codeBytes, id, input);
+        // module.validate_proof(genRec, id);
+        // setText("Proof is valid");
       } catch (e) {
         setText("Error: " + e);
         console.error(e);
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,52 @@
-use risc0_zkvm::{sha::Digest, Receipt};
+use risc0_zkvm::{sha::Digest, Prover, Receipt as Risc0Receipt};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn validate_proof(journal: &[u8], seal: &[u32], image_id: &[u8]) -> Result<(), JsError> {
-    let receipt: Receipt = Receipt::new(journal, seal);
+pub struct Receipt {
+    journal: Box<[u8]>,
+    seal: Box<[u32]>,
+}
+
+#[wasm_bindgen]
+impl Receipt {
+    #[wasm_bindgen(constructor)]
+    pub fn new(journal: Box<[u8]>, seal: Box<[u32]>) -> Self {
+        Self {
+            journal: journal.into(),
+            seal: seal.into(),
+        }
+    }
+    #[wasm_bindgen(getter)]
+    pub fn journal(&self) -> Box<[u8]> {
+        self.journal.clone()
+    }
+    #[wasm_bindgen(getter)]
+    pub fn seal(&self) -> Box<[u32]> {
+        self.seal.clone()
+    }
+}
+
+#[wasm_bindgen]
+pub fn validate_proof(receipt: Receipt, image_id: &[u8]) -> Result<(), JsError> {
+    let receipt = Risc0Receipt::new(&receipt.journal, &receipt.seal);
     let image_id: [u8; 32] = image_id.try_into()?;
     let digest = Digest::from(image_id);
     Ok(receipt
         .verify(&digest)
         .map_err(|e| JsError::new(&format!("Failed to validate proof: {}", e)))?)
 }
+
+// TODO this is currently blocked by getrandom crate not working in wasm. Investigate r0 lib
+// #[wasm_bindgen]
+// pub fn generate_proof(code: &[u8], image_id: &[u8], input: &[u32]) -> Result<Receipt, JsError> {
+//     let image_id: [u8; 32] = image_id.try_into()?;
+//     // TODO this doesn't impl error
+//     let mut prover = Prover::new(code, image_id)
+//         .map_err(|e| JsError::new(&format!("Failed to validate proof: {}", e)))?;
+//     prover.add_input_u32_slice(input);
+//     let receipt = prover
+//         .run()
+//         // TODO this one too
+//         .map_err(|e| JsError::new(&format!("Failed to validate proof: {}", e)))?;
+//     Ok(Receipt::new(receipt.journal.into(), receipt.seal.into()))
+// }

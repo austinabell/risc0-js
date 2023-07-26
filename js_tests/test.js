a915184c03e844066e5603ccd66d1cc372ba22e2
Austin Abell <austinabell8@gmail.com>
2023-07-25 20:36:33 -0400

    add negative tests

--- js_tests/test.js
+++ js_tests/test.js
@@ -1,5 +1,5 @@
 const test = require('node:test');
-// const assert = require('node:assert').strict;
+const assert = require('node:assert').strict;
 const fs = require('fs').promises;
 const { SessionReceipt } = require("../pkg");
 
@@ -13,5 +13,30 @@ async function fetchBinaryFiles() {
 test('example valid proof verification', () => {
 	fetchBinaryFiles().then(([receipt, method_id]) => {
 		SessionReceipt.bincode_deserialize(receipt).validate(method_id);
+	});
+});
+
+test('invalid proof verification should fail with invalid method id', async () => {
+	const [serializedReceipt] = await fetchBinaryFiles();
+	const invalidMethodId = Buffer.alloc(32, 1);
+	const receipt = SessionReceipt.bincode_deserialize(serializedReceipt);
+	assert.throws(() => {
+		receipt.validate(invalidMethodId);
+	}, new Error('Failed to validate proof: image_id mismatch'));
+});
+
+test('invalid proof verification should fail with invalid journal', async () => {
+	const [serializedReceipt, methodId] = await fetchBinaryFiles();
+	// Write a new value as the journal (last bytes of the serialized receipt are the journal)
+	serializedReceipt.writeUInt32LE(123456, serializedReceipt.length - 4);
+	const receipt = SessionReceipt.bincode_deserialize(serializedReceipt);
+	assert.throws(() => {
+		receipt.validate(methodId);
+	}, new Error('Failed to validate proof: Journal digest mismatch detected'));
+});
+
+test('invalid bytes should throw bincode deserialize error', async () => {
+	assert.throws(() => {
+		SessionReceipt.bincode_deserialize(Buffer.alloc(32, 1));
 	});
 });
